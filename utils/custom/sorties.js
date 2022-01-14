71675cda0f27af6bd8af41af5a09275a91d3a338
sean7601 <sean7601@gmail.com>
2022-01-14 10:47:48 -0500

    ingested data

--- utils/custom/sorties.js
+++ utils/custom/sorties.js
@@ -1,1 +1,76 @@
 var sorties = new Object;
+
+sorties.data = {
+    "header": {
+        "title": {
+            "title": "Airplan Title",
+            "subtitle": "Subtitle"
+        },
+        "slap": {
+            "sunrise": "0550",
+            "sunset": "1920",
+            "moonrise": "2120",
+            "moonset": "0440",
+            "moonphase": "25%"
+        },
+        "time": {
+            "flightquarters": "1000",
+            "heloquarters": "0930",
+            "variation": "10 W",
+            "timezone": "L",
+            "sorties": {
+                "day": 1,
+                "night": 0,
+                "total": 1
+            },
+            "hours": {
+                "day": 5,
+                "night": 0,
+                "total": 5
+            }
+        }
+    },
+    "events": {
+        "squadrons": [
+            {
+                "name": "Squadron 1",
+                "cs": "WOLF",
+                "tms": "MH-60R",
+                "modex": "700",
+                "sorties":[
+                    {
+                        "start": 9.5,
+                        "end": 12.5,
+                        "startCycle": null,
+                        "endCycle": null,
+                        "startCondition": "pull",
+                        "endCondition": "stuff",
+                        "annotation": "SSC"
+                    },
+                    {
+                        "start": null,
+                        "end": null,
+                        "startCycle": 1,
+                        "endCycle": 2,
+                        "startCondition": "flyOn",
+                        "endCondition": "flyOff",
+                        "annotation": "LOG"
+                    }
+                ]
+            }
+        ],
+        "cycles": [
+            {
+                "start": 10,
+                "end": 12,
+                "id": 1
+            },
+            {
+                "start": 12.5,
+                "end": 15,
+                "id": 2
+            }
+        ]
+    }
+} 
+        
--- utils/custom/tabular.js
+++ utils/custom/tabular.js
@@ -1,24 +1,27 @@
 var tabular = new Object;
 
 tabular.draw = function(){
-    var sorties = {};//sorties.data;//or wherever the data is stored, may need to change how we loop over it
+    var events = sorties.data.events;//or wherever the data is stored, may need to change how we loop over it
     var html = "<h3>Sortie List</h3>";
     html += "<button class='btn btn-primary mb-2' onclick='sorties.add()'>Add Sortie</button>";
     html += "<table class='table table-striped table-bordered table-condensed'>";
     html += "<thead><tr><th>Sqdrn</th><th>Cycle</th><th>Num</th><th>Off</th><th>On</th></tr></thead>";
     html += "<tbody>";
-    for(var i in sorties){
-        html += "<tr>";
-        html += "<td>"+sorties[i].sqdrn+"</td>";
-        html += "<td>"+sorties[i].cycle+"</td>";
-        html += "<td>"+sorties[i].num+"</td>";
-        html += "<td>"+sorties[i].off+"</td>";
-        html += "<td>"+sorties[i].on+"</td>";
-        html += "<td><button class='btn btn-secondary' onclick='sorties.edit("+i+")'>Edit</button></td>";
-        html += "</tr>";
+    for(var i=0;i<events.squadrons.length;i++){
+        var sqdrn = events.squadrons[i];
+        for(var ii=0;ii<sqdrn.sorties.length;ii++){
+            var sortie = sqdrn.sorties[ii];
+            html += "<tr>";
+            html += "<td>"+sqdrn.name+"</td>";
+            html += "<td>"+sortie.startCycle+"</td>";
+            html += "<td>"+(ii+1)+"</td>";
+            html += "<td>"+sortie.start+"</td>";
+            html += "<td>"+sortie.end+"</td>";
+            html += "<td><button class='btn btn-secondary' onclick='sorties.edit("+i+")'>Edit</button></td>";
+            html += "</tr>";
+        }
     }
     html += "</tbody>";
 
     $("#tabular").html(html);
-
 }
