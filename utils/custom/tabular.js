9c1d2b9633d492054de88926ba25552d2e51116b
Alex Buck <alexjbuck@gmail.com>
2022-01-14 11:53:01 -0500

    add(tab): Add cycle list to tabular view
    Add child div underneath tabular for cycles and sorties.
    
    Add tabular.draw function that calls draw for the cycle and sorties
    
    Change to .forEach instead of explicit index loops.

--- utils/custom/tabular.js
+++ utils/custom/tabular.js
@@ -1,16 +1,20 @@
 var tabular = new Object;
 
-tabular.draw = function(){
+tabular.draw = () => {
+    tabular.sorties.draw()
+    tabular.cycles.draw()
+}    
+
+tabular.sorties = new Object;
+tabular.sorties.draw = () => {
     var events = sorties.data.events;//or wherever the data is stored, may need to change how we loop over it
     var html = "<h3>Sortie List</h3>";
     html += "<button class='btn btn-primary mb-2' onclick='sorties.add()'>Add Sortie</button>";
     html += "<table class='table table-striped table-bordered table-condensed'>";
     html += "<thead><tr><th>Sqdrn</th><th>Cycle</th><th>Num</th><th>Off</th><th>On</th></tr></thead>";
     html += "<tbody>";
-    for(var i=0;i<events.squadrons.length;i++){
-        var sqdrn = events.squadrons[i];
-        for(var ii=0;ii<sqdrn.sorties.length;ii++){
-            var sortie = sqdrn.sorties[ii];
+    events.squadrons.forEach((sqdrn, i) => {
+        sqdrn.sorties.forEach((sortie, ii) => {
             html += "<tr>";
             html += "<td>"+sqdrn.name+"</td>";
             html += "<td>"+sortie.startCycle+"</td>";
@@ -19,9 +23,29 @@ tabular.draw = function(){
             html += "<td>"+sortie.end+"</td>";
             html += "<td><button class='btn btn-secondary' onclick='sorties.edit("+i+")'>Edit</button></td>";
             html += "</tr>";
-        }
-    }
+        })
+    })
     html += "</tbody>";
+    $("#tabular-sorties").html(html);
+}
 
-    $("#tabular").html(html);
+tabular.cycles = new Object;
+tabular.cycles.draw = () => {
+    var cycles = sorties.data.events.cycles;//or wherever the data is stored, may need to change how we loop over it
+    var html = "<h3>Cycle List</h3>";
+    html += "<button class='btn btn-primary mb-2' onclick='cycles.add()'>Add Cycle</button>";
+    html += "<table class='table table-striped table-bordered table-condensed'>";
+    html += "<thead><tr><th>Cycle #</th><th>Start</th><th>End</th></thead>";
+    html += "<tbody>";
+    cycles.forEach((cycle, index) => {
+        html += "<tr>";
+        html += "<td>" + cycle.id + "</td>";
+        html += "<td>" + cycle.start + "</td>";
+        html += "<td>" + cycle.end + "</td>";
+        html += "<td><button class='btn btn-primary' onclick='sorties.edit(" + index + ")'>Edit</button></td>"; // Method needs to be exitCycles or something like that. cycles.edit, whatever works.
+        html += "</tr>";
+        }
+    )
+    html += "</tbody>";
+    $("#tabular-cycles").html(html);
 }
