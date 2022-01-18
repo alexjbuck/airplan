6ae7e80d1137ae357d86431311ac4e4eb7860a50
Alex Buck <alexjbuck@gmail.com>
2022-01-17 21:06:07 -0500

    add(grph): Significant restrucure, nested groups
    Heirarchical nature of groups.
    
    makeStage will create all the needed nested structure.
    
    makeStage calls makePage
    makePageArea calls makePrintArea
    makePrintArea calls makeHeader and makeEvents
    etc...
    
    All the way down. Each make call just fits its content into its parent
    Each make function is abstracted to just that container and its children

--- utils/custom/graphical.js
+++ utils/custom/graphical.js
@@ -1,5 +1,5 @@
-var g = {};
-g.textOptions = {
+let config = new Object();
+config.textOptions = {
   body: {
     fontFamily: 'sans-serif',
     fontSize: 14,
@@ -18,137 +18,237 @@ g.textOptions = {
     align: 'center',
   },
 }
-// Page Margin
-g.m = 10;
+fitStageIntoParentContainer = function() {
+  var container = document.querySelector('#stage');
+  // now we need to fit stage into parent container
+  var containerWidth = container.offsetWidth;
+  // but we also make the full scene visible
+  // so we need to scale all objects on canvas
+  var scale = containerWidth / g.sceneWidth;
+  g.stage.width(g.sceneWidth * scale);
+  g.stage.height(g.sceneHeight * scale);
+  g.stage.scale({ x: scale, y: scale });
+  // console.log("this: ",this)
+  // console.log('Resize event. Stage size: ' + g.stage.width() + 'x' + g.stage.height());
+}
+window.addEventListener('resize', fitStageIntoParentContainer);
 
-// This means that the scene drawn in units of 1/10th inch. 
-// Also forces aspect ratio to be 11:8.5, which is the ratio of the paper size.
-var sceneWidth = 1100;
-var sceneHeight = 850;
+var g = {
+  // Page Margin
+  m: 10,
+  
+  // This means that the scene drawn in units of 1/10th inch. 
+  // Also forces aspect ratio to be 11:8.5, which is the ratio of the paper size.
+  sceneWidth: 1100,
+  sceneHeight: 850,
 
-// first we need to create a stage
-g.stage = new Konva.Stage({
-    container: 'stage',   // id of container <div>
-    width: sceneWidth,
-    height: sceneHeight,
-    name: 'stage',
+  stage: {},
+
+  draw: function() {
+    this.makeStage();
+    console.log('Make Stage');
+  },
+  makeStage: function() {
+    let stage = new Konva.Stage({
+      container: 'stage',   // id of container <div>
+      width: this.sceneWidth,
+      height: this.sceneHeight,
+      name: 'stage',
+    });
+    this.stage = stage;
+    console.log('Draw Stage')
+    stage.pageArea = this.makePageArea(stage);
+    stage.add(stage.pageArea)
+    stage.draw();
+    return stage;
+  },
+  // Page area is the whole page, then add it to stage, draw it, and add a red bounding box
+  makePageArea: function(parent) {
+    console.log('MakePageArea')
+    let pageArea = new Konva.Layer({
+      name: 'pageArea',
+    });
+    pageArea.add( new Konva.Rect({
+      x: 0,
+      y: 0,
+      width: this.sceneWidth,
+      height: this.sceneHeight,
+      stroke: 'red',
+      strokeWidth: 5,
+      name: 'pageAreaBoundary',
+    }))
+    pageArea.printArea = this.makePrintArea(pageArea);
+    pageArea.add(pageArea.printArea);
+    // pageArea.draw()
+    return pageArea;
+  },
+  // Print Area is the printable area, inset by a margin of g.m on all sides, add it to stage, and draw it
+  makePrintArea: function(parent) {
+    console.log('MakePrintArea')
+    let printArea = new Konva.Group({
+      x: this.m,
+      y: this.m,  
+      width: this.sceneWidth-2*this.m,
+      height: this.sceneHeight-2*this.m,
+      name: 'printArea',
+    })
+    printArea.header = this.makeHeader(printArea);
+    printArea.events = this.makeEvents(printArea);
+    printArea.add(printArea.header);
+    printArea.add(printArea.events);
+    printArea.add( new Konva.Rect({
+      x: 0,
+      y: 0,
+      width: printArea.width(),
+      height: printArea.height(),
+      stroke: 'black',
+      strokeWidth: 1,
+      name: 'printAreaBoundary',
+    }))
+    // printArea.draw()
+    return printArea;
+  },
+}
+g.makeHeader = function(parent) {
+  console.log('MakeHeader')
+  let header = new Konva.Group({
     x: 0,
     y: 0,
-});
-
-// Page area is the whole page, then add it to stage, draw it, and add a red bounding box
-g.pageArea = new Konva.Layer({
-  x: 0,
-  y: 0,
-  width: sceneWidth,
-  height: sceneHeight,
-  name: 'pageArea',
-});
-g.stage.add(g.pageArea);
-
-g.pageArea.add( new Konva.Rect({
-    x: 0,
-    y: 0,
-    width: g.pageArea.width(),
-    height: g.pageArea.height(),
-    stroke: 'red',
-    strokeWidth: 5,
-    name: 'pageAreaBoundary',
-  })
-);
-
-// Print Area is the printable area, inset by a margin of g.m on all sides, add it to stage, and draw it
-g.printArea = new Konva.Group({
-  x: g.m,
-  y: g.m,
-  width: g.pageArea.width()-2*g.m,
-  height: g.pageArea.height()-2*g.m,
-  name: 'printArea',
-  })
-g.pageArea.add(g.printArea);
-
-g.printArea.add( new Konva.Rect({
-    x: 0,
-    y: 0,
-    width: g.printArea.width(),
-    height: g.printArea.height(),
+    width: parent.width(),
+    height: parent.height(),
+    name: 'header',
+  });
+  let bottomLine = new Konva.Line({
+    points: [0, header.height(), header.width(), header.height()],
     stroke: 'black',
     strokeWidth: 1,
-    name: 'printAreaBoundary',
-}))
+  });
+  header.slap = this.makeSlap(header);
+  header.title = this.makeTitle(header);
+  header.time = this.makeTime(header);
+  header.add(bottomLine);
+  header.add(header.slap);
+  header.add(header.title);
+  header.add(header.time);
+  // header.draw()
+  return header
+}
+
+g.makeSlap = function(parent) {
+  let slap = new Konva.Group({
+      x: config.textOptions.body.padding,
+      y: config.textOptions.body.padding,
+      name: 'slap_group',
+  })
+  // Label Text
+  var slapLabel = new Konva.Text({
+    text: Object.keys(airplan.data.header.slap).join('\n').toUpperCase(),
+    fontSize: config.textOptions.body.fontSize,
+    align: 'left',
+    fontFamily: config.textOptions.body.fontFamily,
+    name: 'slap_label',
+  });
+  // SLAP Data
+  var slapData = new Konva.Text({
+    text: ['0800L','1730L','1530L','2230L','50% ILLUMINATION'].join('\n'),
+    fontSize: slapLabel.fontSize(),
+    fontFamily: slapLabel.fontFamily(),
+    name: 'slap_data',
+  });
+  slapData.offsetX(-1*(slapLabel.width()+config.textOptions.body.padding));  
+  // Add to Group
+  slap.add(slapLabel);
+  slap.add(slapData);
+  // slap.draw()
+  return slap
+}
+
+g.makeTitle = function(parent) {
+  let title = new Konva.Group({
+    x: parent.width()/2,
+    y: config.textOptions.body.padding,
+    name: 'title_group',
+  })
+  title.offsetX(title.width()/2)
+  // Title Text
+  var titleText = new Konva.Text({
+    text: 'AIR PLAN',
+    fontSize: config.textOptions.title.fontSize,
+    fontFamily: config.textOptions.title.fontFamily,
+    align: config.textOptions.title.align,
+    name: 'title',
+  });
+  // Subtitle Text
+  var subTitleText = new Konva.Text({
+      y: titleText.height() + config.textOptions.subtitle.padding,
+      text: 'CQ Day 1',
+      fontSize: config.textOptions.subtitle.fontSize,
+      fontFamily: config.textOptions.subtitle.fontFamily,
+      align: config.textOptions.subtitle.align,
+      name: 'subtitle',
+  });
+  // Add to Group
+  title.add(titleText);
+  title.add(subTitleText);
+  // title.draw()
+  return title
+}
+
+g.makeTime = function(parent) {
+  let time = new Konva.Group({
+    x: parent.width()-config.textOptions.body.padding,
+    y: config.textOptions.body.padding,
+    name: 'time_group',
+  })
+  time.offsetX(time.width())
+  // Time Label
+  let timeLabel = new Konva.Text({
+    text: ['flight quarters:','Helo quarters','Mag Var:','Time Zone:'].join('\n').toUpperCase(),
+    fontSize: config.textOptions.body.fontSize,
+    fontFamily: config.textOptions.body.fontFamily,
+    align: 'right',
+    name: 'time_label',
+  });
+  // Time Data
+  let timeData = new Konva.Text({
+    text: ['1','2','0','0'].join('\n'),
+    fontSize: timeLabel.fontSize(),
+    fontFamily: timeLabel.fontFamily(),
+    name: 'time_data',
+  });
+  // Add to Group
+  time.add(timeLabel);
+  time.add(timeData);
+  // time.draw()
+  return time
+}
+
+g.makeEvents = function(parent) {
+  let events = new Konva.Group({
+    x: 0,
+    y: parent.header.height(),
+    width: parent.width(),
+    height: parent.height()-parent.header.height(),
+    name: 'events',
+  })
+  return events
+}
 
 // Define Header group, starting at the top left of the printable area, full width, height of 100
 // add it to print area
-g.header = new Konva.Group({
-  x: 0,
-  y: 0,
-  width: g.printArea.width(),
-  height: 100,
-  name: 'header',
-});
-g.printArea.add(g.header);
 
-// Define the three header groups: slap, title, and time
-  // Add the slap group to the header group
-  g.header.add( new Konva.Group({
-    x: g.textOptions.body.padding,
-    y: g.textOptions.body.padding,
-    name: 'slap',
-  }))
-  // Add the title to the header group
-  g.header.add( new Konva.Group({
-    x: g.header.width()/2,
-    y: g.textOptions.body.padding,
-    name: 'title',
-  }))
-  g.stage.find('.title').forEach( (obj) => obj.offsetX(obj.width()/2) )
-  // Add the time group to the header group
-  g.header.add( new Konva.Group({
-    x: g.header.width()-g.textOptions.body.padding,
-    y: g.textOptions.body.padding,
-    name: 'time',
-  }))
-  g.stage.find('.time').forEach( (obj) => obj.offsetX(obj.width()) )
-
-// Define Events group, starting at the bottom left of the header, full width, height of remaining printable area
-// add it to print area
-g.events = new Konva.Group({
-  x: 0,
-  y: 0+g.header.height(),
-  width: g.printArea.width(),
-  height: g.printArea.height()-g.header.height(),
-  name: 'events',
-})
-g.printArea.add(g.events);
-
-// Define the event groups: timeline, squadron, cycle totals
-  // Add the timeline group to the events group
-  g.events.add( g.events.timeline = new Konva.Group({
-    x: 0,
-    y: 0,
-    width: g.events.width(),
-    height: 40,
-    name: 'timeline',
-  }))
-  // Add the cycle totals group to the events group, offsetY to anchor at bottom left corner
-  g.events.add( g.events.cycleTotals = new Konva.Group({
-    x: 0,
-    y: g.events.height(),
-    width: g.events.width(),
-    height: 20,
-    name: 'cycleTotals',
-  }))
-  g.events.cycleTotals.offsetY(g.events.cycleTotals.height())
-  // Add the squadron group to the events group
-  g.events.add( g.events.squadronArea = new Konva.Group({
-    x: 0,
-    y: g.stage.find('.timeline')[0].height(),
-    width: g.events.width(),
-    height: g.events.height()-g.events.timeline.height()-g.events.cycleTotals.height(),
-    name: 'squadronArea',
-  }))
-
-
+g.buildStageTree = () => {
+  g.stage.add(g.pageArea);
+    g.pageArea.add(g.printArea);
+      g.printArea.add(g.header);
+        g.header.add(g.header.slap);
+        g.header.add(g.header.title);
+        g.header.add(g.header.time);
+      g.printArea.add(g.events);
+        g.events.add(g.events.timeline);
+        g.events.add(g.events.squadronArea);
+        g.events.add(g.events.cycleTotals);
+}
 
 g.addHeader = () => {
   var header = new Konva.Layer({
@@ -161,20 +261,20 @@ g.addHeader = () => {
     x: header.width() / 2,
     y: g.m,
     text: 'AIR PLAN',
-    fontSize: g.textOptions.title.fontSize,
-    fontFamily: g.textOptions.title.fontFamily,
-    align: g.textOptions.title.align,
+    fontSize: config.textOptions.title.fontSize,
+    fontFamily: config.textOptions.title.fontFamily,
+    align: config.textOptions.title.align,
   });
   title.offsetX(title.width() / 2);
   header.add(titleText);
 
   var subTitleText = new Konva.Text({
       x: header.width() / 2,
-      y: titleText.height() + g.textOptions.subtitle.padding,
+      y: titleText.height() + config.textOptions.subtitle.padding,
       text: 'CQ Day 1',
-      fontSize: g.textOptions.subtitle.fontSize,
-      fontFamily: g.textOptions.subtitle.fontFamily,
-      align: g.textOptions.subtitle.align,
+      fontSize: config.textOptions.subtitle.fontSize,
+      fontFamily: config.textOptions.subtitle.fontFamily,
+      align: config.textOptions.subtitle.align,
   });
   subTitleText.offsetX(subTitleText.width() / 2);
   header.add(subTitleText);
@@ -209,7 +309,7 @@ g.addTimeText = () => {
   });
   g.timeLayer.add(timeData);
 
-  timeText.offsetX(timeText.width()+timeData.width()+g.textOptions.body.padding);
+  timeText.offsetX(timeText.width()+timeData.width()+config.textOptions.body.padding);
   timeData.offsetX(timeData.width())
 
   g.stage.add(g.timeLayer);
@@ -235,8 +335,8 @@ g.addSquadrons = () => {
     x: squadronBoundingBox.x()+squadronBoundingBox.width()/2,
     y: squadronBoundingBox.y(),
     text: 'squadron'.toUpperCase(),
-    fontSize: g.textOptions.body.fontSize,
-    fontFamily: g.textOptions.body.fontFamily,
+    fontSize: config.textOptions.body.fontSize,
+    fontFamily: config.textOptions.body.fontFamily,
     fontStyle: 'bold',
     align: 'left',
   })
@@ -276,40 +376,4 @@ g.addSquadrons = () => {
 }
 
 
-//
-// DRAW
-//
-g.draw = () => {
-  g.addFrame();
-  g.addTitle();
-  // g.addHeader();
-  g.addSunMoonText();
-  g.addTimeText();
-  g.addSquadrons();
-  console.log('Stage Drawn');
-}
-
-
-g.draw = () => {
-  g.pageArea.draw()
-  g.printArea.draw()
-  g.fitStageIntoParentContainer();
-}
-
-g.fitStageIntoParentContainer = () =>{
-  var container = document.querySelector('#stage');
-
-  // now we need to fit stage into parent container
-  var containerWidth = container.offsetWidth;
-
-  // but we also make the full scene visible
-  // so we need to scale all objects on canvas
-  var scale = containerWidth / sceneWidth;
-  g.stage.width(sceneWidth * scale);
-  g.stage.height(sceneHeight * scale);
-  g.stage.scale({ x: scale, y: scale });
-  console.log('Resize event. Stage size: ' + g.stage.width() + 'x' + g.stage.height());
-}
-
 // adapt the stage on any window resize
-window.addEventListener('resize', g.fitStageIntoParentContainer);
