8cb1f8dbfb92590dffe239f9163b985409d1aaf2
Alex Buck <alexjbuck@gmail.com>
2022-01-26 18:40:27 -0500

    a(ap): Convert to generator. Supports resetting.
    By making the blank airplan a generating function instead of a variable
    we can now reset the app to a blank state without a page refresh.

--- utils/custom/blankAirplan.js
+++ utils/custom/blankAirplan.js
@@ -1,63 +1,50 @@
-var airplan = new Object;
-
-airplan.data = {
-    "date": new Date(),
-    "header": {
-        "title": "Airplan Title",
-        "slap": {
-            "sunrise": new Date(1642675800000),
-            "sunset": new Date(1642724400000),
-            "moonrise": new Date(1650284400000),
-            "moonset": new Date(1650530400000),
-            "moonphase": "25%"
-        },
-        "time": {
-            "flightquarters": new Date(1642675800000),
-            "heloquarters": new Date(1642724400000),
-            "variation": "10 W",
-            "timezone": "L",
-            "sorties": {
-                "day": 1,
-                "night": 0,
-                "total": 1
+BlankAirplan = function() {
+    return {
+        data: {
+            "date": new Date(new Date().setHours(0)),
+            "header": {
+                "title": "Airplan Title",
+                "slap": {
+                    "sunrise": new Date( new Date().setHours(6,30) ),
+                    "sunset": new Date( new Date().setHours(19,0) ),
+                    "moonrise": new Date( new Date().setHours(20) ),
+                    "moonset": new Date( new Date().setHours(2) ),
+                    "moonphase": "%"
+                },
+                "time": {
+                    "flightquarters": new Date(),
+                    "heloquarters": new Date(),
+                    "variation": "",
+                    "timezone": "",
+                    "sorties": {
+                        "day": 1,
+                        "night": 0,
+                        "total": 1
+                    },
+                    "hours": {
+                        "day": 5,
+                        "night": 0,
+                        "total": 5
+                    }
+                }
             },
-            "hours": {
-                "day": 5,
-                "night": 0,
-                "total": 5
+            "events": {
+                "sorties":{
+                },
+                "squadrons": [
+                    {
+                        "name": "Name",
+                        "cs": "Callsign",
+                        "tms": "TMS",
+                        "modex": "MODEX",
+                        "letter": "A",
+                    },
+                ],
+                "cycles": {
+                },
+                "start": new Date( new Date().setHours(8,0) ), // Thu Jan 20 2022 10:30:00 GMT-0500 (Eastern Standard Time)
+                "end": new Date( new Date().setHours(18,0) ), // Thu Jan 20 2022 17:30:00 GMT-0500 (Eastern Standard Time)
             }
-        }
-    },
-    "events": {
-        "sorties":{
-        },
-        "squadrons": [
-            {
-                "name": "VFA-213",
-                "cs": "WOLF",
-                "tms": "MH-60R",
-                "modex": "700",
-                "letter": "A",
-            },
-            {
-                "name": "VFA-201",
-                "cs": "DIMON",
-                "tms": "FA-18E",
-                "modex": "100",
-                "letter": "B",
-            },
-            {
-                "name": "VFA-37",
-                "cs": "TOMCAT",
-                "tms": "FA-18E",
-                "modex": "200",
-                "letter": "C",
-            }
-        ],
-        "cycles": {
-        },
-        "start": new Date(1642692600000), // Thu Jan 20 2022 10:30:00 GMT-0500 (Eastern Standard Time)
-        "end": new Date(1642717800000), // Thu Jan 20 2022 17:30:00 GMT-0500 (Eastern Standard Time)
-    }
-} 
-        
+        } 
+    }  
+}
