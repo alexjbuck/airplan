c0869a2c1cb6a6f74689a68b7e0c0f08c7804a46
Alex Buck <alexjbuck@gmail.com>
2022-01-26 18:39:12 -0500

    a(menu): Add squadron func. Fix dates.

--- utils/custom/menu.js
+++ utils/custom/menu.js
@@ -1,10 +1,18 @@
 var menu = new Object
 
 menu.draw = function() {
-    var html = "<button id='refresh-stage' class='btn btn-primary mx-3' onclick='refresh()'>Refresh View</button>"
-    html += "<label type='file' id='load' class='btn btn-primary my-0 mx-3'>Load Airplan <input type='file' id='filepath' hidden onchange='menu.load(event)'></label>"
-    html += "<button id='save' class='btn btn-primary mx-3' onclick='menu.save()'>Save Airplan</button>"
-    html += "<button id='print' class='btn btn-primary mx-3' onclick='menu.print()'>Print Airplan</button>"
+    var html = "<div class='btn-group mr-5'>";
+    html += "<button class='btn btn-outline-success btn-sm' onclick='menu.addSquadron()'>+</button>";
+    html += "<button class='btn btn-outline-danger btn-sm' onclick='menu.deleteBottomSquadron()'>\u2212</button>";
+    html += "</div>";
+    html += "<div class='btn-group'>";
+    html += "<button id='reset-stage' class='btn btn-outline-danger btn-sm' onclick='menu.reset()'>New Airplan</button>"
+    html += "<button id='refresh-stage' class='btn btn-outline-primary btn-sm' onclick='refresh()'>Refresh View</button>"
+    html += "<label type='file' id='load' class='btn btn-outline-primary btn-sm my-0'>Load Airplan <input type='file' id='filepath' hidden onchange='menu.load(event)'></label>"
+    html += "<button id='save' class='btn btn-outline-primary btn-sm' onclick='menu.save()'>Save Airplan File</button>"
+    html += "<button id='print' class='btn btn-outline-primary btn-sm' onclick='menu.print()'>Print Airplan PDF</button>"
+    html += "</div>"
+    html += "<div class='btn-group'>";
     $('#menu').html(html)
 }
 menu.load = function(e) {
@@ -14,6 +22,8 @@ menu.load = function(e) {
         airplan = JSON.parse(reader.result)
         // convert all Date objects to Date objects
         airplan.data.date = new Date(airplan.data.date)
+        airplan.data.events.start = new Date(airplan.data.events.start)
+        airplan.data.events.end = new Date(airplan.data.events.end)
         airplan.data.header.slap.sunrise = new Date(airplan.data.header.slap.sunrise)
         airplan.data.header.slap.sunset = new Date(airplan.data.header.slap.sunset)
         airplan.data.header.slap.moonrise = new Date(airplan.data.header.slap.moonrise)
@@ -35,14 +45,7 @@ menu.load = function(e) {
 menu.save = function() {
     console.log("Save!")
     let file = new Blob([JSON.stringify(airplan)], {type: "application/json"})
-    let url = URL.createObjectURL(file)
-    let a = document.createElement('a')
-    a.href = url
-    a.id = "airplan-download"
-    a.download = airplan.data.date.toYYYYMMDD()+".json"
-    $('body').append(a)
-    a.click()
-    $('#airplan-download').remove()
+    saveAs(file,airplan.data.date.toYYYYMMDD()+".json")
 }
 
 menu.print = function() {
@@ -53,4 +56,91 @@ menu.print = function() {
     let imgData = g.stage.toDataURL({mimeType: 'image/png', quality: 1, pixelRatio: 3});
     pdf.addImage(imgData, 'JPEG', m*w/2, m*h/2, w*(1-m), h*(1-m), undefined, 'FAST');
     pdf.save('airplan_'+airplan.data.date.toYYYYMMDD()+'.pdf');
-  }
+}
+
+menu.addSquadron = function() {
+    if (airplan.data.events.squadrons.length >= 9) {
+        alert("You can only have 9 squadrons!")
+        return
+    }
+    let html = "<h3>Add Squadron</h3>"
+    html += menu.squadronForm()
+    html += "<button class='btn btn-primary' onclick='menu.addSquadronConfirm()'>Add Squadron</button>"
+    openModal(html)
+    $('#squadron-name').val("Squadron")
+}
+
+menu.editSquadron = function(i) {
+    let html = "<h3>Edit Squadron</h3>"
+    html += menu.squadronForm()
+    html += "<button class='btn btn-primary' onclick='menu.editSquadronConfirm("+i+")'>Update Squadron</button>"
+    openModal(html)
+    $('#squadron-name').val(airplan.data.events.squadrons[i].name)
+    $('#squadron-cs').val(airplan.data.events.squadrons[i].cs)
+    $('#squadron-tms').val(airplan.data.events.squadrons[i].tms)
+    $('#squadron-modex').val(airplan.data.events.squadrons[i].modex)
+}
+
+menu.squadronForm = function() {
+    let html = "<div class='form-group  row align-items-center'>"
+    html += "<label for='squadron-name' class='col-12 col-md-2 text-left text-md-right'>Squadron Name</label>"
+    html += "<input type='text' class='col form-control mr-5' id='squadron-name' placeholder='Squadron Name'>"
+    html += "</div>"
+    html += "<div class='form-group  row align-items-center'>"
+    html += "<label for='squadron-cs' class='col-12 col-md-2 text-left text-md-right'>Callsign</label>"
+    html += "<input type='text' class='col form-control mr-5' id='squadron-cs' placeholder='Callsign'>"
+    html += "</div>"
+    html += "<div class='form-group  row align-items-center'>"
+    html += "<label for='squadron-tms' class='col-12 col-md-2 text-left text-md-right'>TMS</label>"
+    html += "<input type='text' class='col form-control mr-5' id='squadron-tms' placeholder='TMS'>"
+    html += "</div>"
+    html += "<div class='form-group  row align-items-center'>"
+    html += "<label for='squadron-modex' class='col-12 col-md-2 text-left text-md-right'>MODEX</label>"
+    html += "<input type='text' class='col form-control mr-5' id='squadron-modex' placeholder='XXX'>"
+    html += "</div>"
+    return html
+}
+
+    
+
+menu.addSquadronConfirm = function() {
+    let squadron = {
+        name: $('#squadron-name').val(),
+        cs: $('#squadron-cs').val(),
+        tms: $('#squadron-tms').val(),
+        modex: $('#squadron-modex').val(),
+        letter: String.fromCharCode(airplan.data.events.squadrons.length + 65),
+    }
+    airplan.data.events.squadrons.push(squadron)
+    refresh()
+    closeModal()
+}
+
+menu.editSquadronConfirm = function(i) {
+    let squadron = {
+        name: $('#squadron-name').val(),
+        cs: $('#squadron-cs').val(),
+        tms: $('#squadron-tms').val(),
+        modex: $('#squadron-modex').val(),
+        letter: String.fromCharCode(i + 65),
+    }
+    airplan.data.events.squadrons[i] = squadron
+    refresh()
+    closeModal()
+}
+
+menu.deleteBottomSquadron = function() {
+    if (airplan.data.events.squadrons.length <= 0) {
+        alert("There are no squadrons!")
+        return
+    }
+    airplan.data.events.squadrons.pop()
+    refresh()
+}
+
+menu.reset = function() {
+    if(confirm("Are you sure you want to reset the airplan?\nThis will remove all data and reset the airplan to a blank state.")) {
+        airplan = new BlankAirplan()
+        refresh()
+    }
+}
