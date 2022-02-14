8e0e6d9d46f3551976c66ef27107fd5fd3562518
Alex Buck <alexjbuck@gmail.com>
2022-02-13 23:18:57 -0500

    f(tf): Remove public access to s3-website

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -17,3 +17,12 @@ resource "aws_s3_bucket_website_configuration" "root_bucket_website_configuratio
     suffix = "index.html"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "root" {
+  bucket = aws_s3_bucket.root_bucket.id
+
+  block_public_acls   = true
+  block_public_policy = true
+  restrict_public_buckets = true
+  ignore_public_acls   = true
+}
