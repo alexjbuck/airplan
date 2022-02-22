5461b5f0926f70d972111b004bb74d159a631dd9
Alex Buck <alexjbuck@gmail.com>
2022-02-22 11:28:56 -0500

    f(M): Fix and close #25. day/night sortie count.

--- src/app/Sortie.js
+++ src/app/Sortie.js
@@ -24,7 +24,7 @@ class Sortie extends Event {
         return sortie;
     }
     
-    /** @param {String} lineID */
+    /** @param {Line} line */
     set line(line) {
         this.lineID = line.ID;
     }
